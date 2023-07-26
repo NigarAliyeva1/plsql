cf84f4bf56c8b84b8dd04a1a5ac71093793891fb
NigarAliyeva1 <112957859+NigarAliyeva1@users.noreply.github.com>
2023-07-26 16:53:47 +0400

    Create dml.sql

---
+++ procedure/dml.sql
@@ -0,0 +1,11 @@
+insert into bank_countries 
+values(bank_countries_id_seq.nextval,
+'Azerbaijan');
+insert into bank_countries 
+values(bank_countries_id_seq.nextval,
+'Turkey');
+insert into bank_countries 
+values(bank_countries_id_seq.nextval,
+'USA');
+
+commit;
